060b5a44db3e0fb6f55dc0a5826ae4ffa0e73877
luiz-kohler <luizkohler@icloud.com>
2026-01-06 18:03:54 -0300

    applying semaphore to control the request rate to external apis

--- API/Services/FanOutZipcodeResolver/ZipcodeResolver.cs
+++ API/Services/FanOutZipcodeResolver/ZipcodeResolver.cs
@@ -10,35 +10,44 @@ public interface IZipcodeResolver
 public class ZipcodeResolver(IEnumerable<IZipcodeProvider> providers) : IZipcodeResolver
 {
     private readonly IEnumerable<IZipcodeProvider> _providers = providers;
+    private readonly SemaphoreSlim _semaphore = new(5, 5);
     
     public async Task<ZipcodeProviderResponse?> GetZipcodeAddress(string zipcode, CancellationToken cancellationToken)
     {
-        using var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+        await _semaphore.WaitAsync(cancellationToken);
+        
+        try {
+            using var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
 
-        var tasks = _providers.Select(provider =>
-            Task.Run(async () => await provider.GetZipcodeAddress(zipcode, linkedCts.Token), linkedCts.Token)).ToList();
-
-        while (tasks.Count != 0)
-        {
-            var completed = await Task.WhenAny(tasks);
-
-            try
+            var tasks = _providers.Select(provider =>
+                Task.Run(async () => await provider.GetZipcodeAddress(zipcode, linkedCts.Token), linkedCts.Token)).ToList();
+        
+            while (tasks.Count != 0)
             {
-                var result = await completed;
-                if (result != null)
+                var completed = await Task.WhenAny(tasks);
+
+                try
                 {
-                    await linkedCts.CancelAsync();
-                    return result;
+                    var result = await completed;
+                    if (result != null)
+                    {
+                        await linkedCts.CancelAsync();
+                        return result;
+                    }
+                }
+                catch (KeyNotFoundException)
+                {
+                    throw;
+                }
+                finally
+                {
+                    tasks.Remove(completed);
                 }
             }
-            catch (KeyNotFoundException)
-            {
-                throw;
-            }
-            finally
-            {
-                tasks.Remove(completed);
-            }
+        }
+        finally
+        {
+            _semaphore.Release();
         }
         
         throw new Exception($"No zipcode provider found for {zipcode}");
